0a4f9f0292c67e146952e87b37ad3968e14779bc
codecrafter34 <saksham7247620@gmail.com>
2026-06-15 00:54:21 +0530

    mongoatlas

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import mongoose from 'mongoose';
 const connectDb = async() => {
-   try {
-            const conn = await mongoose.connect("mongodb://localhost:27017/chai", {
-                useNewUrlParser: true,
-            });
+    try {
+        const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/chai";
+        const conn = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+        });
             console.log(`MongoDB Connected: ${conn.connection.host}`);
             // return conn
             
